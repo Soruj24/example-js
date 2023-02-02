c24c93968eaa34903ffa877317bcb590b0a2ce8b
soruj <mathsoruj@gmail.com>
2023-02-02 09:23:03 +0600

    first commit

---
+++ big-numbar/index.js
@@ -0,0 +1,9 @@
+function maxInArray(numbars) {
+    for (let i = 0; i < numbars.lenght; i++){
+        const index = i;
+        const element = numbars[index];
+        console.log(element);
+    }
+}
+const heights = [123, 4565, 4, 6, 3, 34, 23, , 546632, 423];
+const tallest = maxInArray(heights)
