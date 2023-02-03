fd4acd3814c2bf5835487b66335e5870145fc388
soruj <mathsoruj@gmail.com>
2023-02-03 20:42:34 +0600

    add

--- big-numbar/index.js
+++ big-numbar/index.js
@@ -1,9 +1,37 @@
-function maxInArray(numbars) {
-    for (let i = 0; i < numbars.lenght; i++){
-        const index = i;
-        const element = numbars[index];
-        console.log(element);
+// const shoppingCard = [
+//     { name: 'shoe', price: 1200 },
+//     { name: 'shirt', price: 2200 },
+//     { name: 'pant', price: 3700 },
+//     { name: 'belt', price: 600 },
+// ];
+
+// function totalCost(products) {
+//     let sum = 0;
+//     for (let i = 0; i < products.length; i++){
+//         const product = products[i]
+//         sum = sum + product.price
+//         console.log(product);
+//     }
+//     return sum;
+// }
+// const expense = totalCost(shoppingCard);
+// console.log(expense);
+
+const shoppingCard = [
+    { name: 'shoe', price: 1200, qontetie:2 },
+    { name: 'shirt', price: 2200, qontetie:4 },
+    { name: 'pant', price: 3700, qontetie:3 },
+    { name: 'belt', price: 600, qontetie:2 },
+];
+
+function totalCost(products) {
+    let sum = 0;
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i]
+        sum = sum + product.price * product.qontetie;
+        console.log(product);
     }
+    return sum;
 }
-const heights = [123, 4565, 4, 6, 3, 34, 23, , 546632, 423];
-const tallest = maxInArray(heights)
+const expense = totalCost(shoppingCard);
+console.log(expense);
