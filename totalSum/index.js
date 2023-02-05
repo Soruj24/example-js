c1309388adaea980dbbe7c4b93afb3925ca82a89
soruj <mathsoruj@gmail.com>
2023-02-05 17:40:51 +0600

    add

--- totalSum/index.js
+++ totalSum/index.js
@@ -1,15 +1,18 @@
 const shoppingCard = [
-    { name: 'shoe', price: 1200 },
-    { name: 'shir', price: 2200 },
-    { name: 'pant', price: 3700 },
-    { name: 'belt', price: 600 },
+    { name: 'shoe', price: 1200, quantity:3 },
+    { name: 'shir', price: 2200 , quantity:7},
+    { name: 'pant', price: 3700, quantity:5 },
+    { name: 'belt', price: 600 , quantity:9},
 ];
 
 function totalCost(products) {
+    let sum = 0;
     for (let i = 0; i < products.length; i++){
         const product = products[i];
-        console.log(product);
+        const productTotal = product.price * product.quantity;
+        sum = sum + productTotal;
     }
+    return sum
 }
 const expense = totalCost(shoppingCard);
 console.log(expense);
